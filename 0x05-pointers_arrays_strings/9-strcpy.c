2b1e3d4d8963821b6786930c95afdd66d186d3f1
Ochadimma <franciscastella86@gmail.com>
2023-06-26 16:14:38 -0700

    stropy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *_stropy - copies the string pointed to by src
+ * _stropy - copies the string pointed to by src
  * @dest: char type string
  * @src: char type string
  * Description: Copy the string poited to by pointer 'src' to
